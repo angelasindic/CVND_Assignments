67fc1a8b80aa187cf9a9e51446ce7476d98bc1fe
angela sindic <asindic@gmail.com>
2018-09-03 17:58:21 +0200

    check in solutions for facial keypoints project

--- facial-keypoint-detection/models.py
+++ facial-keypoint-detection/models.py
@@ -13,25 +13,49 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         
-        ## TODO: Define all the layers of this CNN, the only requirements are:
+        ## Define all the layers of this CNN, the only requirements are:
         ## 1. This network takes in a square (same width and height), grayscale image as input
         ## 2. It ends with a linear layer that represents the keypoints
         ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
         
-        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
-        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv1 = nn.Conv2d(1, 16, 5)    # 220 x 220 x 16
+        self.pool1 = nn.MaxPool2d(2)        # 110 x 110 x 16
+        self.drop1 = nn.Dropout(p=0.2)
         
-        ## Note that among the layers to add, consider including:
-        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        self.conv2 = nn.Conv2d(16, 32, 4)   # 107 x 107 x 32
+        self.pool2 = nn.MaxPool2d(2)        # 53 x  53 x 32
+        self.drop2 = nn.Dropout(p=0.2)
         
-
+        self.conv3 = nn.Conv2d(32, 64, 3)   # 51 x 51 x 64
+        self.pool3 = nn.MaxPool2d(2)        # 25 x 25 x 64
+        self.drop3 = nn.Dropout(p=0.3)
+        
+        self.conv4 = nn.Conv2d(64, 128, 3)  # 23 x 23 x 128
+        self.pool4 = nn.MaxPool2d(2)        # 11 x 11 x 128
+        self.drop4 = nn.Dropout(p=0.3)
+        
+        # dense layers
+        self.fc1 = nn.Linear(11*11*128, 1024)
+        self.fc1_drop = nn.Dropout(p=0.4)
+        self.fc2 = nn.Linear(1024, 1024)
+        self.fc2_drop = nn.Dropout(p=0.3)
+        self.fc3 = nn.Linear(1024, 136)
+        
         
     def forward(self, x):
-        ## TODO: Define the feedforward behavior of this model
-        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        ## x = self.pool(F.relu(self.conv1(x)))
+
+        ## pass x through conv/pool blocks
+        x = self.drop1(self.pool1(F.relu(self.conv1(x))))
+        x = self.drop2(self.pool2(F.relu(self.conv2(x))))
+        x = self.drop3(self.pool3(F.relu(self.conv3(x))))
+        x = self.drop4(self.pool4(F.relu(self.conv4(x))))
+
+        # flatten output before the dense layers
+        x = x.view(x.size(0), -1)
         
-        
-        # a modified x, having gone through all the layers of your model, should be returned
+        # pass throug dense layers
+        x = F.relu(self.fc1(x))
+        x = self.fc1_drop(x)
+        x = self.fc3(x)
+
         return x
